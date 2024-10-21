42bcf0442816e8b1f12897156a1e487ace71f808
olehone <115097045+olehone@users.noreply.github.com>
2024-10-21 21:31:57 +0300

    update ui

--- variables.py
+++ variables.py
@@ -1,5 +1,7 @@
 title = "OTP Manager"
-geometry = "450x500"
+geometry = "700x700"
 qrcodes_path = "qrcodes/"
 app_name = "One-time auth"
 seconds = 30
+padding = {"padx": 5, "pady": 8}
+qr_img_size = 200
--- view.py
+++ view.py
@@ -1,185 +1,208 @@
-import tkinter as tk
+import customtkinter as ctk
 from tkinter import messagebox
 from PIL import Image, ImageTk
+from functools import partial
 from controller import OTPController
-from variables import title, geometry, seconds
+from variables import title, geometry, seconds, padding, qr_img_size
 from datetime import datetime
 
 
-class OTPView(tk.Tk):
+class OTPView:
+
     def __init__(self, controller: OTPController):
-        super().__init__()
         self.controller = controller
-        self.title(title)
-        self.geometry(geometry)
-        self.label_var = tk.StringVar(value="test")
-        key = self.controller.generate_secret_key()
-        self.secret_var = tk.StringVar(value=key)
-        self.otp_type_var = tk.StringVar(value="totp")
-        self.counter_var = tk.StringVar(value="not used")
-        self.qr_label = None
+        self.root = ctk.CTk()
+        self.root.title(title)
+        self.root.geometry(geometry)
+        self.otp_frames = {}
+
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("blue")
+
+        self.label_var = ctk.StringVar(value="Назва")
+        self.secret_var = ctk.StringVar(value=self.controller.generate_secret_key())
+        self.otp_type_var = ctk.StringVar(value="totp")
+        self.counter_var = ctk.StringVar(value="Не використовується")
         self.interval = seconds
+
+        self.qr_label = None
         self.create_widgets()
         self.updating()
 
     def run(self):
-        self.mainloop()
+        self.root.mainloop()
+
+    def update_otp_list(self):
+        time_remaining = int(self.interval - datetime.now().timestamp() % self.interval)
+        otps = self.controller.get_otps()
+
+        labels_to_delete = set(self.otp_frames.keys()) - set(otps.keys())
+
+        for label in labels_to_delete:
+            frame = self.otp_frames.pop(label)
+            frame.destroy()
+        for label, model in otps.items():
+            otp_code = self.controller.generate_otp(model)
+            result = self._format_otp_display(model, otp_code, time_remaining)
+
+            if label in self.otp_frames:
+                existing_frame = self.otp_frames[label]
+                existing_label = existing_frame.winfo_children()[0]
+                existing_label.configure(text=f"{label} - {result}")
+            else:
+                self._create_otp_display(label, model, time_remaining)
+
+    def _create_otp_display(self, label, model, time_remaining):
+        otp_code = self.controller.generate_otp(model)
+        result = self._format_otp_display(model, otp_code, time_remaining)
+
+        frame = ctk.CTkFrame(self.otp_frame)
+        frame.pack(fill="x", padx=5, pady=5)
+
+        label_widget = ctk.CTkLabel(frame, text=f"{label} - {result}")
+        label_widget.pack(side="left")
+
+        self.otp_frames[label] = frame
+        self._create_otp_buttons(frame, label, model.type)
+
+    def _create_otp_buttons(self, frame, label, otp_type):
+        ctk.CTkButton(
+            frame, text="QR-код", command=partial(self.show_qr_code, label)
+        ).pack(side="right")
+
+        ctk.CTkButton(
+            frame, text="Видалити", command=partial(self.delete_otp, label)
+        ).pack(side="right")
+
+        if otp_type == "hotp":
+            ctk.CTkButton(
+                frame, text="Отримати", command=partial(self.count_hotp, label)
+            ).pack(side="right")
+
+    def _format_otp_display(self, model, otp_code, time_remaining):
+        if model.type == "totp":
+            return f"{otp_code}  Часу лишилось: {time_remaining}с"
+        return f"{otp_code}  Лічильник: {model.counter}"
 
     def create_widgets(self):
-        form_frame = tk.Frame(self)
-        form_frame.pack(pady=10)
+        form_frame = ctk.CTkFrame(self.root)
+        form_frame.pack(pady=10, padx=20, fill="both", expand=True)
 
-        tk.Label(form_frame, text="OTP Label:").grid(
-            row=0, column=0, sticky="w", padx=5
+        self._create_label_entry(
+            form_frame, "Назва одноразового паролю:", self.label_var, row=0
         )
-        tk.Entry(form_frame, textvariable=self.label_var).grid(row=0, column=1, padx=5)
+        self._create_label_entry(form_frame, "Секретний ключ:", self.secret_var, row=1)
 
-        tk.Label(form_frame, text="Secret Key:").grid(
-            row=1, column=0, sticky="w", padx=5
+        ctk.CTkLabel(form_frame, text="Тип паролю:").grid(
+            row=2, column=0, **padding, sticky="w"
         )
-        tk.Entry(form_frame, textvariable=self.secret_var, width=40).grid(
-            row=1, column=1, columnspan=2, padx=5, sticky="we"
-        )
-        tk.Label(form_frame, text="OTP Type:").grid(row=2, column=0, sticky="w", padx=5)
-        tk.Radiobutton(
+        self._create_radio_buttons(form_frame)
+
+        self._create_label_entry(
             form_frame,
-            text="time-based",
+            "Лічильник(для залежного від лічильника):",
+            self.counter_var,
+            row=3,
+        )
+
+        ctk.CTkButton(
+            form_frame,
+            text="Згенерувати секретний ключ",
+            command=self.generate_secret_key,
+        ).grid(row=4, column=0, pady=10)
+        ctk.CTkButton(form_frame, text="Додати до списку", command=self.add_otp).grid(
+            row=4, column=1, pady=10
+        )
+
+        self.otp_frame = ctk.CTkFrame(self.root)
+        self.otp_frame.pack(fill="both", expand=True, padx=20, pady=10)
+
+        self.timer_label = ctk.CTkLabel(self.root, text="", font=("Helvetica", 12))
+        self.timer_label.pack()
+
+        self.qr_code_frame = ctk.CTkFrame(self.root)
+        self.qr_code_frame.pack(pady=10)
+
+    def _create_label_entry(self, parent, text, variable, row):
+        ctk.CTkLabel(parent, text=text).grid(row=row, column=0, **padding, sticky="w")
+        entry = ctk.CTkEntry(parent, textvariable=variable)
+        entry.grid(row=row, column=1, **padding, columnspan=2)
+
+    def _create_radio_buttons(self, parent):
+        ctk.CTkRadioButton(
+            parent,
+            text="Залежний від часу",
             variable=self.otp_type_var,
             value="totp",
             command=self.update_counter_visibility,
-        ).grid(row=2, column=1, sticky="w")
-        tk.Radiobutton(
-            form_frame,
-            text="count-based",
+        ).grid(row=2, column=1, **padding)
+        ctk.CTkRadioButton(
+            parent,
+            text="Залежний від лічильника",
             variable=self.otp_type_var,
             value="hotp",
             command=self.update_counter_visibility,
-        ).grid(row=2, column=2, sticky="w")
-
-        tk.Label(form_frame, text="Counter (for HOTP):").grid(
-            row=3, column=0, sticky="w", padx=5
-        )
-        tk.Entry(form_frame, textvariable=self.counter_var).grid(
-            row=3, column=1, padx=5
-        )
-
-        tk.Button(
-            form_frame, text="Generate Secret Key", command=self.generate_secret_key
-        ).grid(row=4, column=0, pady=10)
-        tk.Button(form_frame, text="Add OTP", command=self.add_otp).grid(
-            row=4, column=1, pady=10
-        )
-        self.otp_frame = tk.Frame(self)
-        self.otp_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
-
-        self.timer_label = tk.Label(self, text="", font=("Helvetica", 12))
-        self.timer_label.pack()
-
-        self.qr_code_frame = tk.Frame(self)
-        self.qr_code_frame.pack(pady=10)
+        ).grid(row=2, column=2, **padding)
 
     def update_counter_visibility(self):
-        if self.otp_type_var.get() == "hotp":
-            self.counter_var.set(value="0")
-        else:
-            self.counter_var.set(value="not used")
-
-    def update_otp_list(self):
-        for widget in self.otp_frame.winfo_children():
-            widget.destroy()
-        time_remaining = int(self.interval - datetime.now().timestamp() % self.interval)
-        otps = self.controller.get_otps()
-        for label, model in otps.items():
-            type = model.type
-            frame = tk.Frame(self.otp_frame)
-            frame.pack(fill=tk.X, padx=5, pady=5)
-            code = self.controller.generate_otp(model)
-            if type == "totp":
-                result = f"Code: {code}  Time: {time_remaining}s"
-            else:
-                count = model.counter
-                result = f"Code: {code}  Counter: {count}"
-            label_text = tk.Label(frame, text=f"{label} - Code: {result}")
-            label_text.pack(side=tk.LEFT)
-
-            qr_button = tk.Button(
-                frame,
-                text="Generate QR",
-                command=lambda l=label: self.show_qr_code(l),
-            )
-            qr_button.pack(side=tk.RIGHT)
-            delete_button = tk.Button(
-                frame,
-                text="Delete",
-                command=lambda l=label: self.delete_otp(l),
-            )
-            delete_button.pack(side=tk.RIGHT)
-            if type == "hotp":
-                count_button = tk.Button(
-                    frame, text="Generate", command=lambda l=label: self.count_hotp(l)
-                )
-                count_button.pack(side=tk.RIGHT)
-
-    def clean_fields(self):
-        self.label_var.set("")
-        self.secret_var.set("")
-        self.otp_type_var.set(value="totp")
-        self.counter_var.set(value="0")
+        self.counter_var.set(
+            "0" if self.otp_type_var.get() == "hotp" else "Не використовується"
+        )
 
     def generate_secret_key(self):
-        key = self.controller.generate_secret_key()
-        self.secret_var.set(key)
-        return self.secret_var.get()
+        self.secret_var.set(self.controller.generate_secret_key())
 
     def add_otp(self):
+        label, secret, otp_type, counter = self._get_otp_form_values()
+
+        if not self._validate_input(label, secret, otp_type):
+            return
+
+        self.controller.add_otp(label, secret, otp_type, counter)
+        self.update_otp_list()
+        self.show_qr_code(label)
+
+    def _get_otp_form_values(self):
         label = self.label_var.get().strip()
         secret = self.secret_var.get().strip()
         otp_type = self.otp_type_var.get().strip()
         counter = int(self.counter_var.get()) if otp_type == "hotp" else None
+        return label, secret, otp_type, counter
 
+    def _validate_input(self, label, secret, otp_type):
         if otp_type == "hotp" and not self.controller.is_key_valid(secret):
             messagebox.showwarning(
-                "Invalid Secret Key",
-                "The secret key must be a valid Base32 encoded string.",
+                "Неправильний секретний ключ",
+                "Секретний ключ має бути закодованим рядком Base32.",
             )
-            return
-
+            return False
         if not label or not secret:
             messagebox.showwarning(
-                "Input Error", "Please fill in both the label and secret key."
+                "Помилка Введення", "Будь ласка, введіть назву та ключ"
             )
-            return
-
-        self.controller.add_otp(label, secret, otp_type, counter)
-        self.clean_fields()
-        self.update_otp_list()
-        self.show_qr_code(label)
+            return False
+        return True
 
     def delete_otp(self, label):
         self.controller.delete_otp(label)
         self.update_otp_list()
 
     def show_qr_code(self, label):
-        model = self.controller.get_otp(label)
-        if model:
-            uri = self.controller.get_provisioning_uri(model, label)
-            qr_img = self.controller.generate_qr_code(uri, label)
-            img = qr_img.resize((200, 200), Image.LANCZOS)
-            img = ImageTk.PhotoImage(img)
+        otp = self.controller.get_provisioning_uri(label)
+        if otp:
+            qr_image = self.controller.generate_qr_code(otp)
+            img = ctk.CTkImage(light_image=qr_image, size=[qr_img_size, qr_img_size])
             if self.qr_label:
-                self.qr_label.config(image=img)
+                self.qr_label.configure(image=img)
             else:
-                self.qr_label = tk.Label(self.qr_code_frame, image=img)
-                self.qr_label.pack()
-
+                self.qr_label = ctk.CTkLabel(self.qr_code_frame, image=img, text="")
+                self.qr_label.pack(pady=10)
             self.qr_label.image = img
-
-    def count_hotp(self, label):
-        otp = self.controller.generate_otp(self.controller.otp_manager.otps[label])
-        self.controller.update_hotp_counter(label)
-        self.show_qr_code(label)
-        self.update_otp_list()
 
     def updating(self):
         self.update_otp_list()
-        self.after(1000, self.updating)
+        self.root.after(1000, self.updating)
+
+    def count_hotp(self, label):
+        self.controller.update_hotp_counter(label)
+        self.update_otp_list()
